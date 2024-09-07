a9eeacddd040ed2e858784904948e43b703e591a
Leandro Vandari <bernardokreutz@gmail.com>
2024-09-06 22:06:13 -0300

    Fiz pegar os dados da sala de situação também, organizei o código, e baixei usando multithreading pra acelerar o processo

--- req.py
+++ req.py
@@ -1,62 +1,53 @@
 import requests
-import jwt
-import random
-import math
-import time
 import zipfile
 import io
 import os
 
-auth = jwt.encode({
-  "sub": str(math.floor(random.random() * 1e6)),
-  "iss": "HidroWeb-Front",
-  "permissions": [
-    "read",
-    "write"
-  ],
-  "exp": int(time.time()*10e3)
-}, key=r"7f-j&CKk=coNzZc0y7_4obMP?#TfcYq%fcD0mDpenW2nc!lfGoZ|d?f&RNbDHUX6HIDROWEBBACK", algorithm="HS256", headers={
-  "alg": "HS256",
-  "typ": "JWT"
-} )
+from snirh import headers
+
+def main():
+    end_dir = criar_pasta(os.getcwd(), "DADOS_ESTACOES/")
 
 
-headers = {
+    with open("lista_estacoes.txt", "r", encoding="utf-8") as f:
+        estacoes = f.readlines()
+        tamanho = len(estacoes)
+        for i, estacao in enumerate(estacoes):
 
-"Cookie": '27628ed70890f724c98c0800461fb776=567ac90304fcd78e02f26c27b0eee4e0; c82026e9a6b8da54e034dc38d01ba2a4=922fdfd998f12723e51ababc3b4cccac',
-"Sec-Ch-Ua": '"Chromium";v="125", "Not.A/Brand";v="24"',
-"Sec-Ch-Ua-Mobile": '?0',
-"Authorization": f'Bearer {auth}',
-"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.6422.112 Safari/537.36',
-"Content-Type": 'application/x-www-form-urlencoded',
-"Accept": 'application/json, text/plain, */*',
-"Hidroweb-Front": f'Bearer {auth}',
-"Sec-Ch-Ua-Platform": '"Linux"',
-"Sec-Fetch-Site": 'same-origin',
-"Sec-Fetch-Mode": 'cors',
-"Sec-Fetch-Dest": 'empty',
-"Referer": 'https://www.snirh.gov.br/hidroweb/serieshistoricas',
-"Accept-Encoding": 'gzip, deflate, br',
-"Accept-Language": 'en-US,en;q=0.9',
-"Priority": 'u=1, i',
-}
+            codigo = estacao.split()[0]
+            print(f"Baixando {codigo} -- ({i+1}/{tamanho})\r", end="")
 
-try:
-  end_dir = os.mkdir(os.path.join(os.getcwd(), "DADOS_ESTACOES"))
-except:
-  end_dir = os.path.join(os.getcwd(), "DADOS_ESTACOES")
+            arquivo_snirh = requests.get(
+                "http://www.snirh.gov.br/hidroweb/rest/api/documento/download/files",
+                stream=True,
+                params={
+                    "codigoestacao": codigo,
+                    "tipodocumento": "csv",
+                    "forcenewfiles": "Y",
+                },
+                headers=headers,
+            )
+            arquivo_zip = zipfile.ZipFile(io.BytesIO(arquivo_snirh.content))
 
-with open("lista_estacoes.txt", "r", encoding="utf-8") as f:
-  for estacao in f.readlines():
-    codigo = estacao.split()[0]
-    print(f"Baixando {codigo}")
-    r = requests.get("http://www.snirh.gov.br/hidroweb/rest/api/documento/download/files", stream=True, params={'codigoestacao': codigo, 'tipodocumento': "csv", "forcenewfiles": "Y"}, headers=headers)
-    z = zipfile.ZipFile(io.BytesIO(r.content))
-    try:
-      dir_estacao = os.mkdir(os.path.join(end_dir, str(codigo)))
-    except:
-      dir_estacao = os.path.join(end_dir, str(codigo))
-    z.extractall(dir_estacao)
-    if not os.listdir(dir_estacao):
-      os.rmdir(dir_estacao)
+            dir_estacao = criar_pasta(end_dir, str(codigo))
+            dir_snirh = criar_pasta(dir_estacao, "snirh")
+
+            arquivo_zip.extractall(dir_snirh)
+
+            arquivo_sala_de_situação = requests.get(
+                f"https://saladesituacao.rs.gov.br/api/station/ana/sheet/{codigo}",
+            )
+            with open(os.path.join(dir_estacao, "sala_de_situacao.xlsx"), "wb") as f:
+                f.write(arquivo_sala_de_situação.content)
+
+def criar_pasta(pai, nome) -> str:
+    pasta = os.path.join(pai, nome)
+    os.makedirs(pasta, exist_ok=True)
+
+    return pasta
+
+if __name__ == "__main__":
+    main()
+
+
 
---
+++ thread_pool_req.py
@@ -0,0 +1,60 @@
+import requests
+import zipfile
+import io
+import os
+from concurrent.futures import ThreadPoolExecutor
+from threading import Lock
+
+from snirh import headers
+
+def baixar(enumeracao):
+    idx, codigo = enumeracao
+    print(f"Baixando {codigo} -- ({idx})", end="\r")
+    arquivo_snirh = requests.get(
+        "http://www.snirh.gov.br/hidroweb/rest/api/documento/download/files",
+        stream=True,
+        params={
+            "codigoestacao": codigo,
+            "tipodocumento": "csv",
+            "forcenewfiles": "Y",
+        },
+        headers=headers,
+    )
+    arquivo_zip = zipfile.ZipFile(io.BytesIO(arquivo_snirh.content))
+
+    dir_estacao = criar_pasta(end_dir, str(codigo))
+    dir_snirh = criar_pasta(dir_estacao, "snirh")
+
+    arquivo_zip.extractall(dir_snirh)
+
+    arquivo_sala_de_situação = requests.get(
+        f"https://saladesituacao.rs.gov.br/api/station/ana/sheet/{codigo}",
+    )
+    with open(os.path.join(dir_estacao, "sala_de_situacao.xlsx"), "wb") as f:
+        f.write(arquivo_sala_de_situação.content)
+
+
+def main():
+    with open("lista_estacoes.txt", "r", encoding="utf-8") as f:
+        estacoes = f.readlines()
+        print(f"Total: {len(estacoes)} estações")
+
+        codigos = enumerate([estacao.split()[0] for estacao in estacoes])
+        with ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(baixar, codigos)
+
+            
+
+def criar_pasta(pai, nome) -> str:
+    pasta = os.path.join(pai, nome)
+    os.makedirs(pasta, exist_ok=True)
+
+    return pasta
+
+end_dir = criar_pasta(os.getcwd(), "DADOS_ESTACOES/")
+
+if __name__ == "__main__":
+    main()
+
+
+
